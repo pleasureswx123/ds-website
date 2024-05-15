3c6784bfeba2a9d2e52c96780b3114df4e9fb5c8
从未、淡定 <swe_ling@163.com>
2024-05-15 15:02:22 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,11 +11,11 @@ export default defineNuxtConfig({
   experimental: {
     renderJsonPayloads: false
   },
-  runtimeConfig: {
-    "app": {
-      "cdnURL": isDev ? '' : "http://123.56.169.72"
-    },
-  },
+  // runtimeConfig: {
+  //   "app": {
+  //     "cdnURL": isDev ? '' : "http://123.56.169.72"
+  //   },
+  // },
   modules: [
     "@vueuse/nuxt",
     "@element-plus/nuxt",
