df75e1c71da294a0db35eb69a91d25e9bd6a87d3
shangwenxue <pleasureswx123@163.com>
2024-05-16 16:54:32 +0800

    feat: update

--- pages/apply/[id]/index.vue
+++ pages/apply/[id]/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container p-8 m-auto">
     <div class="flex gap-8 md:flex-row flex-col" v-if="detailInfo">
-      <div class="md:w-2/5 w-full  text-gray-600 body-font shrink-0">
+      <div class="md:w-2/5 w-full  text-gray-600 shrink-0">
         <img class="w-full h-60 object-cover object-center rounded" :src="baseApi + detailInfo.thumbnail"
           alt="thumbnail">
         <div class="py-6 px-3">
@@ -11,7 +11,7 @@
           <h1 class="title-font text-lg font-medium text-gray-900 mb-3">{{ detailInfo.name }}</h1>
           <p class="leading-relaxed mb-3 line-clamp-10 text-ellipsis text-justify">{{ detailInfo.content }}</p>
           <div class="flex items-center flex-wrap ">
-            <a class="text-red-500 inline-flex items-center md:mb-2 lg:mb-0">查看详情
+            <a class="text-red-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer" @click="handleJumpTo">查看详情
               <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none"
                 stroke-linecap="round" stroke-linejoin="round">
                 <path d="M5 12h14"></path>
@@ -25,74 +25,100 @@
           </div>
         </div>
       </div>
-      <div class="grow">
-        <div class="body-font text-gray-900 font-medium text-2xl">
+      <div class="grow space-y-6">
+        <div class="text-gray-900 space-y-2 text-base border rounded p-3 shadow-lg ring ring-red-200 bg-red-100">
           <div>项目名称：{{ detailInfo.project.projectName }}</div>
           <div>大赛名称：{{ detailInfo.name }}</div>
           <div>比赛城市：{{ detailInfo.cityId }}</div>
-          <div>比赛时间：{{ detailInfo.createTime }} - {{ detailInfo.endTime }}</div>
+          <div>比赛时间：{{ detailInfo.createTime }} 至 {{ detailInfo.endTime }}</div>
         </div>
-        <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" label-width="auto" class="demo-ruleForm"
-          :size="formSize" status-icon :scroll-to-error="true">
-          <el-form-item label="姓名" prop="username">
-            <el-input v-model="ruleForm.username" placeholder="请输入姓名" />
-          </el-form-item>
+        <UDivider label="请填写报名信息" :ui="{ label: 'text-red-500' }" />
+        <el-form ref="ruleFormRef" label-position="top" :model="ruleForm" :rules="rules" label-width="auto" size="large"
+          status-icon :scroll-to-error="true">
+          <div class="flex gap-5 flex-col md:flex-row">
+            <el-form-item class="grow" label="姓名" prop="nickName">
+              <el-input v-model="ruleForm.nickName" placeholder="请输入姓名" />
+            </el-form-item>
+            <el-form-item class="grow" label="手机号" prop="phoneNumber">
+              <el-input v-model="ruleForm.phoneNumber" placeholder="请输入手机号"></el-input>
+            </el-form-item>
+          </div>
+          <div class="flex gap-5 flex-col md:flex-row flex-wrap">
 
-          <el-form-item label="年龄" prop="age">
-            <el-input-number v-model.number="ruleForm.age" :min="18" :max="100" :step="1" step-strictly
-              controls-position="right"><template #decrease-icon>
-                <el-icon>
-                  <Minus />
-                </el-icon>
-              </template>
-              <template #increase-icon>
-                <el-icon>
-                  <Plus />
-                </el-icon>
-              </template>
-            </el-input-number>
-          </el-form-item>
+            <el-form-item class="grow shrink-0 w-[100px]" label="邮箱" prop="email">
+              <el-input v-model="ruleForm.email" placeholder="请输入邮箱"></el-input>
+            </el-form-item>
 
-          <el-form-item label="性别" prop="gender">
-            <el-radio-group v-model="ruleForm.gender">
-              <el-radio value="1">男</el-radio>
-              <el-radio value="2">女</el-radio>
-            </el-radio-group>
-          </el-form-item>
+            <el-form-item class="flex-none" label="年龄" prop="age">
+              <el-input-number class="!w-full" v-model.number="ruleForm.age" :min="18" :max="100" :step="1"
+                step-strictly controls-position="right"><template #decrease-icon>
+                  <el-icon>
+                    <Minus />
+                  </el-icon>
+                </template>
+                <template #increase-icon>
+                  <el-icon>
+                    <Plus />
+                  </el-icon>
+                </template>
+              </el-input-number>
+            </el-form-item>
 
-          <el-form-item label="手机号" prop="phoneNumber">
-            <el-input v-model="ruleForm.phoneNumber" placeholder="请输入手机号"></el-input>
-          </el-form-item>
+            <el-form-item class="flex-none" label="类型" prop="teamType">
+              <el-segmented v-model="ruleForm.teamType" :options="typeOptions" />
+            </el-form-item>
 
-          <el-form-item label="邮箱" prop="email">
-            <el-input v-model="ruleForm.email" placeholder="请输入邮箱"></el-input>
+            <el-form-item class="flex-none" label="性别" prop="gender">
+              <el-radio-group v-model="ruleForm.gender">
+                <el-radio :value="1">男</el-radio>
+                <el-radio :value="2">女</el-radio>
+              </el-radio-group>
+            </el-form-item>
+          </div>
+          <el-form-item label="个人介绍" prop="introduce">
+            <el-input v-model="ruleForm.introduce" maxlength="100" placeholder="请输入内容" show-word-limit
+              type="textarea" />
           </el-form-item>
-
-          <el-form-item label="类型" prop="type">
-            <el-segmented v-model="ruleForm.type" :options="typeOptions" />
+          <el-form-item label="比赛经验" prop="experience">
+            <el-input v-model="ruleForm.experience" maxlength="100" placeholder="请输入内容" show-word-limit
+              type="textarea" />
           </el-form-item>
-          <el-form-item label="个人介绍" prop="desc1">
-            <el-input v-model="ruleForm.desc1" maxlength="100" placeholder="请输入内容" show-word-limit type="textarea" />
+          <el-form-item label="目标/期望" prop="expectation">
+            <el-input v-model="ruleForm.expectation" maxlength="100" placeholder="请输入内容" show-word-limit
+              type="textarea" />
           </el-form-item>
-          <el-form-item label="比赛经验" prop="desc2">
-            <el-input v-model="ruleForm.desc2" maxlength="100" placeholder="请输入内容" show-word-limit type="textarea" />
+          <el-form-item label="补充说明" prop="other">
+            <el-input v-model="ruleForm.other" maxlength="100" placeholder="请输入内容" show-word-limit type="textarea" />
           </el-form-item>
-          <el-form-item label="目标/期望" prop="desc3">
-            <el-input v-model="ruleForm.desc3" maxlength="100" placeholder="请输入内容" show-word-limit type="textarea" />
-          </el-form-item>
-          <el-form-item label="补充说明" prop="desc4">
-            <el-input v-model="ruleForm.desc4" maxlength="100" placeholder="请输入内容" show-word-limit type="textarea" />
-          </el-form-item>
-
-          <!-- <el-form-item label="Instant delivery" prop="delivery">
-        <el-switch v-model="ruleForm.delivery" />
-      </el-form-item> -->
+          <template v-if="detailInfo.project.isWorks === 'Y'">
+            <el-form-item label="上传图片作品">
+              <div class="flex flex-wrap gap-3">
+                <img class="size-[100px] object-container bg-gray-200" v-for="(pic, index) in ruleForm.imgList"
+                  :key="index" :src="pic" alt="">
+              </div>
+              <div class="block w-full mt-3"></div>
+              <div class="relative">
+                <UInput type="file" size="sm" icon="i-heroicons-folder" />
+                <input class="cursor-pointer bg-red-600 w-full h-full opacity-0 absolute top-0 left-0" type="file"
+                  accept="image/*" @change="handleFileChange($event, 'img')">
+              </div>
+            </el-form-item>
+            <el-form-item label="上传视频作品">
+              <div class="relative">
+                <UInput type="file" size="sm" icon="i-heroicons-folder" />
+                <input class="cursor-pointer bg-red-600 w-full h-full opacity-0 absolute top-0 left-0" type="file"
+                  accept="video/*" @change="handleFileChange($event, 'video')">
+              </div>
+            </el-form-item>
+          </template>
         </el-form>
 
-        <el-button type="primary" @click="submitForm(ruleFormRef)">
-          报名
-        </el-button>
-        <el-button @click="resetForm(ruleFormRef)">重置</el-button>
+        <div class="grid grid-cols-2 gap-4">
+          <UButton size="md" block color="rose" @click="submitForm(ruleFormRef)" label="报名" />
+          <UButton size="md" block color="gray" @click="resetForm(ruleFormRef)" label="重置" />
+        </div>
+
+
       </div>
     </div>
   </div>
@@ -100,36 +126,125 @@
 
 <script setup>
 import { Minus, Plus } from "@element-plus/icons-vue";
+const handleFileChange = async (e, type) => {
+  const file = e.target.files[0];
+  const formData = new FormData();
+  formData.append("file", file);
+  const result = await $fetch(`${baseApi}/common/upload`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${curToken.value}`,
+    },
+    body: formData,
+  }).catch((err) => err.data);
+  const { code, url } = result;
+  if (code === 200) {
+    if (type === 'img') {
+      ruleForm.imgList = [...ruleForm.imgList, url];
+    }
+    if (type === 'video') {
+      ruleForm.videoList = [...ruleForm.videoList, url];
+    }
+  }
+}
 const ruleFormRef = ref();
-const formSize = ref("default");
 const ruleForm = reactive({
-  username: "",
+  nickName: "",
   phoneNumber: "",
   email: "",
-  delivery: false,
   gender: "",
-  type: "",
-  desc1: "",
-  desc2: "",
-  desc3: "",
-  desc4: "",
+  teamType: "",
+  introduce: "",
+  experience: "",
+  expectation: "",
+  other: "",
+  imgList: [],
+  videoList: [],
 });
+
+const userInfoStore = useUserInfoStore();
+
 const paramsInfo = computed(() => {
-  const { projectId, projectName, contestId, name, cityId, createTime, endTime } = detailInfo || {};
-  return { ...resetForm, ...{ projectId, projectName, contestId, name, cityId, createTime, endTime } }
-})
+  const userId = userInfoStore?.user?.userId
+  const { projectId, contestId, cityId, beginTime, endTime } = detailInfo.value || {};
+
+  const { imgList, videoList, ...formInfo } = ruleForm;
+  return {
+    userId,
+    ...formInfo,
+    worksPic: imgList.join(','),
+    worksVideo: videoList.join(','),
+    ...{ projectId, contestId, cityId, beginTime, endTime },
+    // ...{
+    //   phonenumber: '',
+    //   signupId: null,
+    //   createBy: null,
+    //   createTime: null,
+    //   updateBy: null,
+    //   updateTime: null,
+    // }
+  }
+});
+
+
+const toast = useToast();
+
+const submitForm = async formEl => {
+  if (!formEl) return;
+  await formEl.validate(async (valid, fields) => {
+    if (valid) {
+      const result = await $fetch(`${baseApi}/system/signup`, {
+        method: "POST",
+        body: paramsInfo.value,
+        headers: {
+          Authorization: `Bearer ${curToken.value}`,
+        },
+      });
+      const { code } = result || {};
+      console.log(1112234, result);
+      if (code === 200) {
+        toast.add({
+          timeout: 0,
+          color: "lime",
+          // icon: "i-heroicons-check-circle",
+          title: '提示',
+          description: '您已经报名成功了！',
+          actions: [
+            {
+              variant: 'solid', color: 'red', label: '知道了',
+              click: () => {
+                userInfoStore.setUserInfo();
+                navigateTo("/");
+              }
+            },
+            // {
+            //   variant: 'outline', color: 'red', label: '取消',
+            //   click: () => { }
+            // }
+          ],
+          callback: () => {
+            console.log(999)
+          }
+        });
+      }
+    } else {
+      console.log("error submit!", fields);
+    }
+  });
+};
+
 const typeOptions = [
-  { label: "个人性质", value: 1 },
-  { label: "单位性质", value: 2 },
+  { label: "个人性质", value: 0 },
+  { label: "单位性质", value: 1 },
 ];
 const rules = reactive({
-  username: [{ required: true, message: "请输入姓名", trigger: "blur" }],
+  nickName: [{ required: true, message: "请输入姓名", trigger: "change" }],
   phoneNumber: [
-    { required: true, message: "请输入手机号", trigger: "blur" },
-    { pattern: /^1[3-9]\d{9}$/, message: "手机号格式不正确", trigger: "blur" },
+    { required: true, message: "请输入手机号", trigger: "change" },
+    { pattern: /^1[3-9]\d{9}$/, message: "手机号格式不正确", trigger: "change" },
   ],
   email: [
-    { required: true, message: "请输入邮箱", trigger: "blur" },
+    { required: true, message: "请输入邮箱", trigger: "change" },
     { type: "email", message: "邮箱格式不正确", trigger: ["blur", "change"] },
   ],
   gender: [
@@ -139,25 +254,18 @@ const rules = reactive({
       trigger: "change",
     },
   ],
-  type: [
+  teamType: [
     {
       required: true,
       message: "请选择类型",
       trigger: "change",
     },
   ],
-  desc1: [{ required: true, message: "请输入内容", trigger: "blur" }],
+  introduce: [{ required: true, message: "请输入内容", trigger: "change" }],
+  experience: [{ required: true, message: "请输入内容", trigger: "change" }],
+  expectation: [{ required: true, message: "请输入内容", trigger: "change" }],
+  other: [{ required: true, message: "请输入内容", trigger: "change" }],
 });
-const submitForm = async formEl => {
-  if (!formEl) return;
-  await formEl.validate((valid, fields) => {
-    if (valid) {
-      console.log("submit!");
-    } else {
-      console.log("error submit!", fields);
-    }
-  });
-};
 
 const resetForm = (formEl) => {
   if (!formEl) return;
@@ -174,9 +282,13 @@ const curToken = computed(() => {
 });
 const detailInfo = ref(null);
 
-
 const contestId = route.params.id;
 const projectId = ref();
+
+
+const handleJumpTo = () => {
+  navigateTo(`/join/${contestId}`);
+}
 
 
 const getDetail = async (id) => {
@@ -197,9 +309,6 @@ const getDetail = async (id) => {
 }
 
 getDetail(contestId);
-
-
-
 </script>
 
 <style lang="scss" scoped></style>
