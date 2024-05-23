93ec7715b929d92fc8e103522bff499a129be48b
shangwenxue <pleasureswx123@163.com>
2024-05-23 18:03:15 +0800

    feat: update

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-wrap select-none">
     <template v-if="!!projectList.length">
-      <div class="p-4 md:w-1/3 hover:cursor-pointer" v-for="item in projectList" :key="item.projectId"  @click="jumpTo(item.projectId)">
+      <div class="p-4 md:w-1/3 hover:cursor-pointer" v-for="item in projectList" :key="item.projectId"  @click="goDetail(item)">
         <div class="shadow-md ring-1 ring-inset ring-gray-200 rounded-lg overflow-hidden transition duration-150 ease-out hover:ease-in hover:shadow-2xl">
           <div class="w-full aspect-[2/1] overflow-hidden">
             <img class="w-full h-full object-cover object-center transition duration-150 ease-out hover:ease-in hover:scale-110" :src="getStaticPath(item.thumbnail)" alt="image" />
@@ -11,7 +11,7 @@
             <h1 class="text-lg font-medium text-gray-900 mb-3 text-red-600 text-justify">{{item.projectName}}</h1>
             <p class="leading-relaxed mb-3 line-clamp-5 text-ellipsis text-justify">{{item.content}}</p>
             <div>
-              <UButton label="查看详情" @click="jumpTo(item.projectId)">
+              <UButton label="查看详情">
                 <template #trailing>
                   <UIcon name="i-heroicons-arrow-right-20-solid" class="w-5 h-5" />
                 </template>
@@ -28,7 +28,7 @@
 </template>
 
 <script setup>
-import {Picture} from "@element-plus/icons-vue";
+// import {Picture} from "@element-plus/icons-vue";
 const appStoreInfo = useAppStoreInfo();
 const {projectList} = storeToRefs(appStoreInfo);
 appStoreInfo.getProjectList();
