29ab5bdcbbd64d6a2803f3b725061a52892130d2
shangwenxue <pleasureswx123@163.com>
2024-05-23 18:43:57 +0800

    feat: update

--- pages/index/components/Banner.vue
+++ pages/index/components/Banner.vue
@@ -2,7 +2,7 @@
   <UCarousel ref="carouselRef" v-slot="{ item }" :items="imgsList" :ui="{ item: 'basis-full' }"
              class="overflow-hidden rounded-lg" indicators>
     <div class="w-full cursor-pointer">
-      <div class="aspect-[2/1] bg-gray-100 overflow-hidden relative" @click="jumpTo(item.id)">
+      <div class="aspect-[2/1] bg-gray-100 overflow-hidden relative" @click="jumpTo(item.to)">
         <img :src="item.img" class="w-full h-full object-cover object-center" draggable="false">
         <div class="absolute w-full bottom-0 left-0 bg-gray-900/[.3] text-white font-bold p-4 pb-10 text-center bg-op">{{item.name}}</div>
       </div>
@@ -12,25 +12,48 @@
 
 <script setup>
 import {computed} from "vue";
-
 const appStoreInfo = useAppStoreInfo();
-const {projectList} = storeToRefs(appStoreInfo);
-const {getProjectList} = appStoreInfo;
+const {getProjectList, getContestList, getNotificationList, getBroadcastList} = appStoreInfo;
+const [projectList, contestList, notificationList, broadcastList] = await Promise.all([getProjectList(), getContestList(), getNotificationList(), getBroadcastList()]);
 
-await getProjectList();
-
-const jumpTo = id => {
-  navigateTo(`/about/${id}`)
+const jumpTo = path => {
+  path && navigateTo(path);
 }
 
 const imgsList = computed(() => {
-  return (projectList.value || []).filter(item => item.carousel === 'Y').map(item =>  {
+  return [(projectList || []).map(item => {
     return {
+      carousel: item.carousel || '',
       id: item.projectId,
+      to: `/about/${item.projectId}`,
       img: getStaticPath(item.thumbnail),
       name: item.projectName
     }
-  })
+  }), (contestList || []).map(item => {
+    return {
+      carousel: item.carousel || '',
+      id: item.contestId,
+      to: `/join/${item.contestId}`,
+      img: getStaticPath(item.thumbnail),
+      name: item.name
+    }
+  }), (notificationList || []).map(item => {
+    return {
+      carousel: item.carousel || '',
+      id: item.notificationId,
+      to: `/notice/${item.notificationId}`,
+      img: getStaticPath(item.thumbnail),
+      name: item.notificationName
+    }
+  }), (broadcastList || []).map(item => {
+    return {
+      carousel: item.carousel || '',
+      id: item.broadcastId,
+      to: `/broadcast/${item.broadcastId}`,
+      img: getStaticPath(item.thumbnail),
+      name: item.broadcastName
+    }
+  })].flat().filter(item => item.carousel === 'Y')
 })
 
 const carouselRef = ref()
